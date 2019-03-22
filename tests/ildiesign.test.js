47ef5f9470807d124134adfa72826d4ff06c69cc
Andrei Gaspar <andrei@planeeet.com>
2019-03-22 19:42:51 +0200

    - Headless browser and node update

--- tests/ildiesign.test.js
+++ tests/ildiesign.test.js
@@ -6,11 +6,11 @@ let browser, page;
 
 beforeEach( async () => {
     browser = await puppeteer.launch({
-        headless: false
+        headless: true
     });
 
     page = await browser.newPage();
-    await page.goto( "localhost:3000" );
+    await page.goto( "http://localhost:3000" );
 });
 
 afterEach( async () => {
