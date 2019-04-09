7972994039cafc27bec3beee50a360d8b44f29fd
Andrei Gaspar <andrei@planeeet.com>
2019-04-09 22:28:11 +0300

    - Updated website copy and copy to clipboard function [ci skip]

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -22,7 +22,46 @@ window.onload = () => {
     copyEmailBtn.addEventListener( "click", function () {
         copyEmailBtn.innerText = "Copied";
         copyEmailBtn.classList.add( "copied" );
-        navigator.clipboard.writeText( "ildiko!ildiesign".replace( '!', '@' ) + "moc.".split('').reverse().join('') );
+        copyToClipboard( "ildiko!ildiesign".replace( '!', '@' ) + "moc.".split('').reverse().join('') );
     });
 
+
+
+    
+    function copyToClipboard(str) {
+        let el = document.createElement('textarea');
+        el.value = str;
+        el.setAttribute('readonly', '');
+        el.style = {position: 'absolute', left: '-9999px'};
+        document.body.appendChild(el);
+
+        if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+            // save current contentEditable/readOnly status
+            let editable = el.contentEditable;
+            let readOnly = el.readOnly;
+
+            // convert to editable with readonly to stop iOS keyboard opening
+            el.contentEditable = true;
+            el.readOnly = true;
+
+            // create a selectable range
+            let range = document.createRange();
+            range.selectNodeContents(el);
+
+            // select the range
+            let selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(range);
+            el.setSelectionRange(0, 999999);
+
+            // restore contentEditable/readOnly to original state
+            el.contentEditable = editable;
+            el.readOnly = readOnly;
+        } else {
+            el.select();
+        }
+
+        document.execCommand('copy');
+        document.body.removeChild(el);
+    }
 };
