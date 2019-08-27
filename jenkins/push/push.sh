4e04ab418bb2bd8f4e922e784e4438195e157379
Andrei Gaspar <andrei@planeeet.com>
2019-08-27 16:08:19 +0300

    - Service update

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -10,11 +10,9 @@ docker login -u gasparandr -p $PASS
 
 echo "************* Tagging Images ******************"
 
-docker tag ildiesign-nginx:latest gasparandr/ildiesign-nginx:latest
 docker tag ildiesign-node:latest gasparandr/ildiesign-node:latest
 
 echo "************* Pushing Images ******************"
 
-docker push gasparandr/ildiesign-nginx:latest
 docker push gasparandr/ildiesign-node:latest
 
